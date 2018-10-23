fe667bfe017b9f64623992eb56fe4e6ac48c3e79
marion1998 <marion281098@outlook.fr>
2018-10-23 16:14:26 +0200

    test avec Vue, ajout d'un nouveau component 'test.vue'

---
+++ exo/papaly/src/components/Test.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="test">
+    <h3><b>Hey, regarde mon </b> 
+    <a href="https://github.com/marion1998" target="_blank" rel="noopener">Github</a>
+    </h3>
+    <h1> {{ msg }} </h1>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Test',
+    props: {
+        msg: String
+    } 
+}
+</script>
+
