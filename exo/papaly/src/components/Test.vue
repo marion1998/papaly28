872fe28c4c77afb963f7483e1b14e62292071a41
Gyozmo <binantguillaume@gmail.com>
2018-10-25 13:41:24 +0200

    Increment works

--- exo/papaly/src/components/Test.vue
+++ exo/papaly/src/components/Test.vue
@@ -3,16 +3,17 @@
     <h3><b>Hey, regarde mon </b> 
     <a href="https://github.com/marion1998" target="_blank" rel="noopener">Github</a>
     </h3>
-    <h1> {{ msg }} </h1>
+    <button @click="">test</button>
+    <h1> the button : {{ counter }} </h1>
 </div>
 </template>
 
 <script>
 export default {
     name: 'Test',
-    props: {
-        msg: String
-    } 
+    data() {
+        return {counter: 0}
+    }
 }
 </script>
 
